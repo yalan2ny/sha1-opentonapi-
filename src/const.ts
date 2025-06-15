9fca49209e9c4e0cce8bd03e881ac4d647309c9e
yalan2ny <152720661+yalan2ny@users.noreply.github.com>
2025-06-15 07:50:29 +0330

    Initialize v4_sdk

---
+++ src/const.ts
@@ -0,0 +1,8 @@
+const contentMaxWidth = 1160;
+const animationTimeout = 250;
+
+const githubLink = "https://github.com/orbs-network/ton-contract-verifier";
+
+const SEARCH_HISTORY = "searchHistory";
+
+export { contentMaxWidth, animationTimeout, githubLink, SEARCH_HISTORY };
