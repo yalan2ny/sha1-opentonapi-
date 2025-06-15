ff6b27e573f0a0663e48d28052981a0fc08f9794
yalan2ny <152720661+yalan2ny@users.noreply.github.com>
2025-06-15 07:49:22 +0330

    Initialize bugfix/files-table

---
+++ src/components/Footer.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  ContributedWrapper,
+  CredentialsWrapper,
+  FooterLink,
+  FooterTextBoxLeft,
+  FooterTextBoxRight,
+  FooterWrapper,
+  Separator,
+  SocialsContent,
+  SocialsWrapper,
+} from "./footer.styled";
+import { AppLogo, LinkWrapper } from "./topbar.styled";
+import { Typography, useMediaQuery } from "@mui/material";
+import heart from "../assets/heart.svg";
+import telegram from "../assets/telegram.svg";
+import telegramHovered from "../assets/telegram-hover.svg";
+import github from "../assets/github-footer.svg";
+import githubHovered from "../assets/github-hover.svg";
+import { HoverableIcon } from "./HoverableIcon";
+import icon from "../assets/icon.svg";
+import { useNavigate } from "react-router-dom";
+import { CenteringBox } from "./common.styled";
+
+export const TELEGRAM_SUPPORT_LINK = "https://t.me/tonverifier";
+
+export function Footer() {
+  const isExtraSmallScreen = useMediaQuery("(max-width: 450px)");
+  const navigate = useNavigate();
+
+  return (
+    <FooterWrapper>
+      <SocialsWrapper
+        mb={isExtraSmallScreen ? 5 : "inherit"}
+        sx={{
+          flexDirection: isExtraSmallScreen ? "column" : "inherit",
+          alignItems: isExtraSmallScreen ? "center" : "inherit",
+        }}>
+        <CenteringBox>
+          <LinkWrapper sx={{ color: "#000" }} onClick={() => navigate("/")}>
+            <img src={icon} alt="App icon" width={30} height={30} />
+            <AppLogo>
+              TON VERIFIER <span style={{ fontWeight: 700, fontSize: 14 }}>Beta</span>
+            </AppLogo>
+          </LinkWrapper>
+        </CenteringBox>
+        <SocialsContent>
+          <HoverableIcon
+            iconUrl={telegram}
+            hoveredIconUrl={telegramHovered}
+            link={TELEGRAM_SUPPORT_LINK}
+          />
+          <HoverableIcon
+            iconUrl={github}
+            hoveredIconUrl={githubHovered}
+            link="https://github.com/orbs-network/ton-contract-verifier"
+          />
+        </SocialsContent>
+      </SocialsWrapper>
+      <Separator />
+      <CredentialsWrapper>
+        <FooterTextBoxLeft>
+          <Typography variant="body2">© 2022 Orbs.com</Typography>
+        </FooterTextBoxLeft>
+        <ContributedWrapper>
+          <Typography variant="body2">
+            Contributed with {` `}
+            <img src={heart} alt="Orbs logo" width={9} height={7} />
+            {` `} by {` `}
+            <FooterLink sx={{ color: "#CF84D1" }} target="_blank" href="https://orbs.com/">
+              Orbs
+            </FooterLink>
+          </Typography>
+        </ContributedWrapper>
+        <FooterTextBoxRight>
+          <FooterLink target="_blank" href={TELEGRAM_SUPPORT_LINK}>
+            <Typography variant="body2">Support</Typography>
+          </FooterLink>
+        </FooterTextBoxRight>
+      </CredentialsWrapper>
+    </FooterWrapper>
+  );
+}
