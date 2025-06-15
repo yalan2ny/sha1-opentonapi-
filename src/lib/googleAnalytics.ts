8cdee97725015066eabbeb83267e3f5cbade679d
yalan2ny <152720661+yalan2ny@users.noreply.github.com>
2025-06-15 07:50:30 +0330

    Initialize tact_deploye

---
+++ src/lib/googleAnalytics.ts
@@ -0,0 +1,48 @@
+import ReactGA from "react-ga4";
+
+export enum AnalyticsAction {
+  ADD_FILE = "ADD_FILE",
+  CONNECT_WALLET_POPUP = "CONNECT_WALLET_POPUP",
+  WALLET_CONNECTED = "WALLET_CONNECTED",
+  SELECT_WALLET = "SELECT_WALLET",
+  COMPILE_SUBMIT = "COMPILE_SUBMIT",
+  COMPILE_SERVER_ERROR = "COMPILE_SERVER_ERROR",
+  SIGN_SERVER_ERROR = "SIGN_SERVER_ERROR",
+  SIGN_SERVER_SUCCESS = "SIGN_SERVER_SUCCESS",
+  COMPILE_HASHES_NOT_SIMILAR = "COMPILE_HASHES_NOT_SIMILAR",
+  COMPILE_COMPILATION_ERROR = "COMPILE_COMPILATION_ERROR",
+  COMPILE_SUCCESS_HASHES_MATCH = "COMPILE_SUCCESS_HASHES_MATCH",
+  PUBLISH_CLICK = "PUBLISH_CLICK",
+  TRANSACTION_ISSUED = "TRANSACTION_ISSUED",
+  TRANSACTION_REJECTED = "TRANSACTION_REJECTED",
+  TRANSACTION_ERROR = "TRANSACTION_ERROR",
+  TRANSACTION_EXPIRED = "TRANSACTION_EXPIRED",
+  CONTRACT_DEPLOYED = "CONTRACT_DEPLOYED",
+  IN_BROWSER_COMPILE_ERROR = "IN_BROWSER_COMPILE_ERROR",
+  IN_BROWSER_COMPILE_START = "IN_BROWSER_COMPILE_START",
+  IN_BROWSER_COMPILE_SUCCESS = "IN_BROWSER_COMPILE_SUCCESS",
+  GETTER_PARSE_START = "GETTER_PARSE_START",
+  RUN_GETTER = "RUN_GETTER",
+}
+
+export const sendAnalyticsEvent = (action: AnalyticsAction, label: string = "") => {
+  if (!ReactGA.isInitialized) {
+    return;
+  }
+  try {
+    ReactGA.event({
+      category: "VERIFIER",
+      action,
+      label,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const initGA = () => {
+  try {
+    ReactGA.initialize(import.meta.env.VITE_APP_GA!);
+    ReactGA.send(window.location.pathname + window.location.search);
+  } catch (error) {}
+};
---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import ContractInteract from "./components/admin/ContractInteract";
+import "./index.css";
+import { SnackbarProvider } from "notistack";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import { Admin } from "./components/admin/Admin";
+import { initGA } from "./lib/googleAnalytics";
+import { TactDeployer } from "./components/tactDeployer/TactDeployer";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
+initGA();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <SnackbarProvider maxSnack={3}>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/interact" element={<ContractInteract />} />
+            <Route path="/admin" element={<Admin />} />
+            {/* <Route path="/tactDeployer" element={<TactDeployer />} /> */}
+            <Route path="/:contractAddress" element={<App />} />
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
+  </SnackbarProvider>,
+);
