d9798774db39004b85f6e129dc289cd5fe4de694
yalan2ny <152720661+yalan2ny@users.noreply.github.com>
2025-06-15 07:50:36 +0330

    Initialize add-license-1

---
+++ src/lib/useLoadContractProof.ts
@@ -0,0 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
+import { Sha256 } from "@aws-crypto/sha256-js";
+import { useLoadContractInfo } from "./useLoadContractInfo";
+import "@ton-community/contract-verifier-sdk";
+import { SourcesData } from "@ton-community/contract-verifier-sdk";
+import { useContractAddress } from "./useContractAddress";
+import { usePublishProof } from "./usePublishProof";
+
+export const toSha256Buffer = (s: string) => {
+  const sha = new Sha256();
+  sha.update(s);
+  return Buffer.from(sha.digestSync());
+};
+
+export async function getProofIpfsLink(hash: string): Promise<string | null> {
+  return ContractVerifier.getSourcesJsonUrl(hash, {
+    verifier: window.verifierId,
+    testnet: window.isTestnet,
+  });
+}
+
+export function useLoadContractProof() {
+  const { contractAddress } = useContractAddress();
+  const { data: contractInfo, error: contractError } = useLoadContractInfo();
+  const { status: publishProofStatus } = usePublishProof();
+  const { isLoading, error, data, refetch } = useQuery<
+    Partial<SourcesData> & {
+      hasOnchainProof: boolean;
+    }
+  >(
+    [contractAddress, "proof"],
+    async () => {
+      if (!contractAddress) {
+        return {
+          hasOnchainProof: false,
+        };
+      }
+
+      const ipfsLink = await getProofIpfsLink(contractInfo!.codeCellHash.base64);
+
+      if (!ipfsLink) {
+        return { hasOnchainProof: false, ipfsLink };
+      }
+
+      const sourcesData = await ContractVerifier.getSourcesData(ipfsLink, {
+        testnet: window.isTestnet,
+      });
+      return {
+        hasOnchainProof: true,
+        ...sourcesData,
+      };
+    },
+    {
+      enabled:
+        !!contractAddress &&
+        !!contractInfo?.codeCellHash.base64 &&
+        publishProofStatus === "initial",
+      retry: 2,
+    },
+  );
+
+  return { isLoading, error: error ?? contractError, data, refetch };
+}
