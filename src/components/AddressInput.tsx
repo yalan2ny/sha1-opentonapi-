f7e79e13535dbaef64a18c93d8ab44b8f71d5bcb
yalan2ny <152720661+yalan2ny@users.noreply.github.com>
2025-06-15 07:48:29 +0330

    Initialize hotfix/fonts

---
+++ src/components/AddressInput.tsx
@@ -0,0 +1,32 @@
+import "./AddressInput.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useContractAddress } from "../lib/useContractAddress";
+
+function AddressInput() {
+  const { contractAddress } = useContractAddress();
+  const [val, setVal] = useState(contractAddress ?? "");
+
+  useEffect(() => {
+    setVal(contractAddress ?? "");
+  }, [contractAddress]);
+
+  const navigate = useNavigate();
+  return (
+    <input
+      className="AddressInput"
+      type="search"
+      value={val}
+      onChange={(e: any) => {
+        setVal(e.target.value);
+      }}
+      onKeyDown={(e: any) => {
+        if (e.keyCode === 13) {
+          navigate(`/${e.target.value}`);
+        }
+      }}
+    />
+  );
+}
+
+export default AddressInput;
