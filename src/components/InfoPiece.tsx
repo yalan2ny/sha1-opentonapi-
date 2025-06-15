e48a1bb7bc3109a445e2bdc5d1367f13618710d9
yalan2ny <152720661+yalan2ny@users.noreply.github.com>
2025-06-15 07:49:27 +0330

    Initialize bugfix/selected-tab

---
+++ src/components/InfoPiece.tsx
@@ -0,0 +1,13 @@
+//TODO replace with DataRow
+import "./InfoPiece.css";
+
+function InfoPiece({ label, data }: { label: string; data: string }) {
+  return (
+    <div className="InfoPiece">
+      <div className="InfoPiece-Label">{label}</div>
+      <div className="InfoPiece-Data">{data}</div>
+    </div>
+  );
+}
+
+export default InfoPiece;
